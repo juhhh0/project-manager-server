e0662ca6dce0d2e9b592d2e9495a7332c590105d
Joris Mansion <juhh@juhh-core.lan>
2024-06-23 23:57:03 +0200

    init apollo server

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+const projects = [
+    {
+        title: "To do list",
+    },
+    {
+        title: "Facebook clone",
+    },
+];
+const typeDefs = `#graphql
+  type Project {
+    title: String
+  }
+  
+  type Query {
+    projects: [Project]
+  }
+`;
+const resolvers = {
+    Query: {
+        projects: () => projects,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
